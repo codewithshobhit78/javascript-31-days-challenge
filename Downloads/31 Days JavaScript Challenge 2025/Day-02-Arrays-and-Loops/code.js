2a518584975eac63fff6cdd885e4bf427545c1d7
Shobhit Kumar <sobhitkumar42@gmail.com>
2025-09-16 11:27:50 +0530

    Day 2 – Arrays & Loops added ✅

---
+++ Downloads/31 Days JavaScript Challenge 2025/Day-02-Arrays-and-Loops/code.js
@@ -0,0 +1,21 @@
+// Day 2 – Arrays & Loops 🚀
+
+// Array banate hain
+let fruits = ["🍎 Apple", "🍌 Banana", "🥭 Mango", "🍇 Grapes"];
+
+// For loop use karke print karte hain
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+// For-of loop (short aur easy)
+for (let fruit of fruits) {
+  console.log("I like " + fruit);
+}
+
+// While loop example
+let count = 0;
+while (count < 3) {
+  console.log("Count is: " + count);
+  count++;
+}
